7eafc93c632bff4edf173dfe3fb821965287e89e
André <andreluismoura22@gmail.com>
2023-07-19 21:53:41 -0300

    mod: modified components to implement custom hook and animations

--- fe/src/components/Loader/index.jsx
+++ fe/src/components/Loader/index.jsx
@@ -4,15 +4,19 @@ import { Overlay } from './styles.js';
 
 import Spinner from '../Spinner';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount.js';
 
 export default function Loader({ isLoading }) {
-	if(!isLoading) {
+
+	const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+	if(!shouldRender) {
 		return null;
 	}
 
 	return (
 		<ReactPortal containerId='loader-root'>
-			<Overlay>
+			<Overlay ref={animatedElementRef} isLeaving={!isLoading}>
 				<Spinner size={90}/>
 			</Overlay>,
 		</ReactPortal>
