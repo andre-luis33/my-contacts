150f4248f802fd19d421b13013372755240eb85c
André <andreluismoura22@gmail.com>
2023-07-19 21:50:23 -0300

    add: custom hook animated unmount

---
+++ fe/src/hooks/useAnimatedUnmount.js
@@ -0,0 +1,42 @@
+import propTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
+
+export default function useAnimatedUnmount(visible) {
+
+	const [shouldRender, setShouldRender] = useState(visible);
+
+	const animatedElementRef = useRef(null);
+
+	useEffect(() => {
+		if(visible) {
+			setShouldRender(true);
+		}
+
+		function handleAnimationEnd() {
+			setShouldRender(false);
+		}
+
+		const elementRef = animatedElementRef.current;
+
+		if(!visible && elementRef) {
+			elementRef.addEventListener('animationend', handleAnimationEnd);
+		}
+
+		return () => {
+			if(elementRef) {
+				elementRef.removeEventListener('animationend', handleAnimationEnd);
+			}
+		};
+
+	}, [visible]);
+
+	return {
+		shouldRender,
+		animatedElementRef
+	};
+
+}
+
+useAnimatedUnmount.propTypes = {
+	visible: propTypes.bool.isRequired
+};
