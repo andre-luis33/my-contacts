7551e7c65bd1bddd3742c2c25b09d92e44799c6a
André Luís <andreluismoura22@gmail.com>
2024-03-21 12:09:11 -0300

    fix: db connection

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -2,25 +2,10 @@ const { Client } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-let config;
+const client = new Client({
+	connectionString: process.env.DB_CONNECTION_STRING
+});
 
-if(process.env.NODE_ENV !== 'development') {
-	config = {
-		connectionString: process.env.DB_CONNECTION_STRING
-	};
-
-} else {
-	config = {
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME
-	};
-}
-
-
-const client = new Client(config);
 client.connect();
 
 exports.query = async (query, values) => {
